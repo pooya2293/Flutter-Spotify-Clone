b4fce836eb3695af2ae17c5d3d8a85dc51088185
Pouriya <poorya2293@gmail.com>
2026-08-01 21:54:23 +0330

    Add SignupPage and theme palette for dark mode

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:client/core/theme/app_palette.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Palette.backgroundColor,
+  );
+}
